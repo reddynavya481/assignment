ddcc100e6ddecb6c6f08c11a5cc1d461cf4a5090
reddynavya481 <reddynavya481@gmail.com>
2020-03-13 18:39:24 +0530

    added app.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+
+import SignUp from './SignUp/SignUp'
+class App extends Component{
+  render(){
+    return(
+      <div align="center">
+        <h2 align="center">Track Your Activities</h2>
+      <SignUp/>
+      </div>
+    )
+  }
+}
+
+export default App;
